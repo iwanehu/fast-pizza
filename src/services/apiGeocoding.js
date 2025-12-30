d5b9f8766d992fcf5834148dce2ba9a7674c9731
iwanehu <120640856+iwanehu@users.noreply.github.com>
2025-12-31 00:47:21 +0100

    Refactor getAddress to use Nominatim API
    Updated the getAddress function to use Nominatim API for reverse geocoding with improved error handling and headers.

--- src/services/apiGeocoding.js
+++ src/services/apiGeocoding.js
@@ -1,3 +1,4 @@
+/*
 export async function getAddress({ latitude, longitude }) {
   const res = await fetch(
 	`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`,
@@ -6,4 +7,29 @@ export async function getAddress({ latitude, longitude }) {
 
   const data = await res.json();
   return data;
+}
+
+*/
+
+export async function getAddress({ latitude, longitude }) {
+  try {
+    const res = await fetch(
+      `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`,
+      {
+        headers: {
+          'Accept-Language': 'es', // dirección en español
+          'User-Agent': 'MiPortafolioSnayder/1.0' // Identifícate para evitar bloqueos
+        }
+      }
+    );
+
+    if (!res.ok) throw new Error("Error al obtener la dirección");
+    const data = await res.json();
+    
+    
+    return data; 
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
